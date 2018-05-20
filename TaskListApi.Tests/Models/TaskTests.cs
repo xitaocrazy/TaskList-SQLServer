47e81cc12507a83ceec2a169e2457ccb98dc6d53
Daniel de Souza Martins <daniel.martins@way2.com.br>
2018-05-20 14:54:54 -0300

    Adding classes to build Unit Tests
    - Adding classes to build Unit Tests;
    - Tests do Task class done.

---
+++ TaskListApi.Tests/Models/TaskTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using TaskListApi.Models;
+
+namespace TaskListApi.Tests.Models {
+    [TestFixture]
+    public class TaskTests {
+        private Task _task1;
+        private Task _task2;
+        private Task _task3;
+        private DateTime _data;
+
+        [SetUp]
+        public void SetUp() {
+            _data = DateTime.Now;
+            _task1 = Builder<Task>.CreateNew()
+                .With(t => t.Id = 1)
+                .With(t => t.Title = "Task 1")
+                .With(t => t.Status = true)
+                .With(t => t.Description = "Task 1")
+                .With(t => t.Creation = _data)
+                .With(t => t.LasUpdate = null)
+                .With(t => t.Exclusion = null)
+                .With(t => t.Conclusion = null)
+                .Build();
+            _task2 = Builder<Task>.CreateNew()
+                .With(t => t.Id = 2)
+                .With(t => t.Title = "Task 2")
+                .With(t => t.Status = true)
+                .With(t => t.Description = "Task 2")
+                .With(t => t.Creation = _data)
+                .With(t => t.LasUpdate = null)
+                .With(t => t.Exclusion = null)
+                .With(t => t.Conclusion = null)
+                .Build();
+            _task3 = Builder<Task>.CreateNew()
+                .With(t => t.Id = 1)
+                .With(t => t.Title = "Task 1")
+                .With(t => t.Status = true)
+                .With(t => t.Description = "Task 1")
+                .With(t => t.Creation = _data)
+                .With(t => t.LasUpdate = null)
+                .With(t => t.Exclusion = null)
+                .With(t => t.Conclusion = null)
+                .Build();
+        }
+
+        [Test]
+        public void ToString_should_return_the_expected_value() {
+            var expected =
+                $"Title: Task 1 - Description: Task 1 - Status: On going - Creation: {_data} - LasUpdate:  - Exclusion:  - Conclusion: ";
+            var actual = _task1.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetHashCode_should_return_the_expected_value() {
+            var expected = GetHashCode(_task1);
+            var actual = _task1.GetHashCode();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ObjectEquals_should_indicate_that_are_equals() {
+            var areEquals = Equals(_task1, _task3);
+            Assert.IsTrue(areEquals);
+        }
+
+        [Test]
+        public void ObjectEquals_should_indicate_that_are_differents() {
+            var areEquals = Equals(_task1, _task2);
+            Assert.IsFalse(areEquals);
+        }
+
+        [Test]
+        public void Equals_should_indicate_that_are_equals() {
+            var areEquals = _task1.Equals(_task3);
+            Assert.IsTrue(areEquals);
+        }
+
+        [Test]
+        public void Equals_should_indicate_that_are_differents() {
+            var areEquals = _task1.Equals(_task2);
+            Assert.IsFalse(areEquals);
+        }
+
+        [Test]
+        public void Equality_operator_should_indicate_that_are_equals() {
+            var areEquals = _task1 == _task3;
+            Assert.IsTrue(areEquals);
+        }
+
+        [Test]
+        public void Equality_operator_should_indicate_that_are_differents() {
+            var areEquals = _task1 == _task2;
+            Assert.IsFalse(areEquals);
+        }
+
+        [Test]
+        public void Difference_operator_should_indicate_that_are_differents() {
+            var areDifferents = _task1 != _task2;
+            Assert.IsTrue(areDifferents);
+        }
+
+        [Test]
+        public void Difference_operator_should_indicate_that_are_not_differents() {
+            var areDifferents = _task1 != _task3;
+            Assert.IsFalse(areDifferents);
+        }
+
+        private static int GetHashCode(Task task) {
+            unchecked {
+                var hashCode = task.Id.GetHashCode();
+                hashCode = (hashCode * 397) ^ task.Title?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ task.Status.GetHashCode();
+                hashCode = (hashCode * 397) ^ task.Description?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ task.Creation.GetHashCode();
+                hashCode = (hashCode * 397) ^ task.LasUpdate?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ task.Exclusion?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ task.Conclusion?.GetHashCode() ?? 0;
+                return hashCode;
+            }
+        }
+    }
+}
--- TaskListApi/Models/Task.cs
+++ TaskListApi/Models/Task.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace TaskListApi.Models {
-    public class Task {
+    public class Task : IEquatable<Task> {
         [Key]
         public int Id { get; set; }
         [Required]
@@ -18,5 +18,55 @@ namespace TaskListApi.Models {
         public DateTime? LasUpdate { get; set; }
         public DateTime? Exclusion { get; set; }
         public DateTime? Conclusion { get; set; }
+
+        public override string ToString() {
+            var active = Status ? "On going" : "Done";
+            return
+                $"Title: {Title} - Description: {Description} - Status: {active} - Creation: {Creation} - LasUpdate: {LasUpdate} - Exclusion: {Exclusion} - Conclusion: {Conclusion}";
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                var hashCode = Id.GetHashCode();
+                hashCode = (hashCode * 397) ^ Title?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ Status.GetHashCode();
+                hashCode = (hashCode * 397) ^ Description?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ Creation.GetHashCode();
+                hashCode = (hashCode * 397) ^ LasUpdate?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ Exclusion?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ Conclusion?.GetHashCode() ?? 0;
+                return hashCode;
+            }
+        }
+
+        public virtual bool Equals(Task other) {
+            return other != null &&
+                   Id == other.Id &&
+                   String.Equals(Title, other.Title) &&
+                   Status == other.Status &&
+                   String.Equals(Description, other.Description) &&
+                   Creation == other.Creation &&
+                   DateTime.Equals(LasUpdate, other.LasUpdate) &&
+                   DateTime.Equals(Exclusion, other.Exclusion) &&
+                   DateTime.Equals(Conclusion, other.Conclusion);
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) {
+                return false;
+            }
+            if (ReferenceEquals(this, obj)) {
+                return true;
+            }
+            return obj.GetType() == GetType() && Equals((Task) obj);
+        }
+
+        public static bool operator ==(Task a, Task b) {
+            return Equals(a, b);
+        }
+
+        public static bool operator !=(Task a, Task b) {
+            return !Equals(a, b);
+        }
     }
 }
